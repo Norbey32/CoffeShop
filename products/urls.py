967fbe3ab2489221163492084338e2df34d116c6
Norbey Gutierrez <norbeyd_gutierrez@soy.sena.edu.co>
2025-09-22 19:28:54 -0500

    agregar una endponits para la api

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from products.views import  ProductListView, ProductFormView
+from products.views import  ProductListView, ProductListAPI, ProductFormView
 
 
 urlpatterns = [
     path("", ProductListView.as_view(), name="list_product"),
+    path("api/", ProductListAPI.as_view(), name="list_product_api"),
     path("agregar/", ProductFormView.as_view(), name="add_product"),
 ]
